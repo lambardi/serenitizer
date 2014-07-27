c5adcc74e797eb6b3064576e1b07845a9eefb20c
Ben Golden <ben@sendgrid.com>
2014-07-27 12:57:51 -0600

    cleaned up regex expressions to be more readable and removed some active record dependencies in spec_helper

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -28,29 +28,29 @@ class Snippet
     to_be_translated = self.text.downcase
 
     # goram
-    to_be_translated = to_be_translated.gsub(/[gG][oO][dD]\s[dD][aA][mM][mMnN]/, "goram")
+    to_be_translated = to_be_translated.gsub(/god\sdam[mMnN]*/, "goram")
 
     # replace thing with bunger
     to_be_translated = to_be_translated.gsub(/thing/, "bunger")
 
     # drop the 'g' on gerunds
-    to_be_translated = to_be_translated.gsub(/[iI][nN][gG]/, "in'")
+    to_be_translated = to_be_translated.gsub(/ing/, "in'")
 
     # use the word ain't
-    to_be_translated = to_be_translated.gsub(/[aA][rR][eE][nN]['][tT]/, "ain't")
-    to_be_translated = to_be_translated.gsub(/[aA][rR][eE]\s[nN][oO][tT]/, "ain't")
+    to_be_translated = to_be_translated.gsub(/are[']*n[']*t/, "ain't")
+    to_be_translated = to_be_translated.gsub(/are\snot/, "ain't")
     to_be_translated = to_be_translated.gsub(/are\syou\snot/, "ain't you")
 
 
     # use don't instead of doesn't or does not
-    to_be_translated = to_be_translated.gsub(/[dD][oO][eE][sS][']*[nN][']*[tT]/, "don't")
-    to_be_translated = to_be_translated.gsub(/[dD][oO][eE][sS]\s[nN][oO][tT]/, "don't")
+    to_be_translated = to_be_translated.gsub(/does[']*n[']*t/, "don't")
+    to_be_translated = to_be_translated.gsub(/does\snot/, "don't")
 
     # what the hell -> what in the sphincter of hell
-    to_be_translated = to_be_translated.gsub(/[wW][hH][aA][tT]\s[iI][nN]\s[tT][hH][eE]\s[hH][eE][lL]{2}/, "what in the sphincter of hell")
+    to_be_translated = to_be_translated.gsub(/what\sin\sthe\shell/, "what in the sphincter of hell")
 
     # no ly on advectives
-    to_be_translated = to_be_translated.gsub(/[lL][yY]/, "")
+    to_be_translated = to_be_translated.gsub(/ly/, "")
 
     #
 
